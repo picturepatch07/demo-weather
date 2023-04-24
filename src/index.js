022fa1f3a7f2a93f26619f1fc7dd483a6655712b
Abhishek Anand <anand2000ys@gmail.com>
2023-04-24 12:38:25 +0530

    api added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Alert } from "react-native";
+import React, { useState } from "react";
+import { Constants } from "expo-constants";
+
+const API_KEYS = "dd42ed0c9fd2d490e96487736f09ed02";
 
 const Weather = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  // add a function to fetch the weather data
+  const fetchWeather = async (cityName) => {
+    try {
+      setLoaded(false);
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEYS}&units=metric`
+      );
+      if (response.status == 200) {
+        const data = await response.json();
+        setWeatherData(data);
+      } else {
+        setWeatherData(null);
+      }
+      setLoaded(true);
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -16,8 +40,19 @@ export default Weather;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#FCF5DB",
+    width: "100%",
+    //paddingTop: Constants.statusBarHeight,
+  },
+  header: {
     alignItems: "center",
+    backgroundColor: "#C5D2EF",
+    height: 80,
     justifyContent: "center",
+  },
+  headerTitle: {
+    paddingTop: 20,
+    fontSize: 29,
+    fontWeight: "bold",
   },
 });
